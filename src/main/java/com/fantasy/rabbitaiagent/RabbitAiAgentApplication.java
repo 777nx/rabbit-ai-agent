2ea4cf69a9aed6a0f32bd2163e9fd181304af9e2
Fantasy <1362724990@qq.com>
2025-08-09 22:28:42 +0800

    feat: RAG 知识库 1. Spring AI + 本地知识库，开发文档加载器，给 AI 恋爱大师应用新增和知识库对话功能 2. Spring AI + 云知识库服务，开发云知识库 RAG Advisor 3. 整合 PgVector 实现向量存储 4. 基于 Token 的文本分割器 5. 基于 AI 的关键词增强器 6. 基于 AI 的多查询扩展器 7. 基于 AI 的查询重写器 8. 给恋爱大师文档加载器补充 status 状态的自动提取 9. 自定义检索增强顾问（工厂模式），实现更灵活地搜索 10. 自定义上下文查询增强器（工厂模式），提高容错性

--- src/main/java/com/fantasy/rabbitaiagent/RabbitAiAgentApplication.java
+++ src/main/java/com/fantasy/rabbitaiagent/RabbitAiAgentApplication.java
@@ -1,9 +1,11 @@
 package com.fantasy.rabbitaiagent;
 
+import com.fantasy.rabbitaiagent.rag.PgVectorVectorStoreConfig;
+import org.springframework.ai.autoconfigure.vectorstore.pgvector.PgVectorStoreAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = PgVectorStoreAutoConfiguration.class)
 public class RabbitAiAgentApplication {
 
     public static void main(String[] args) {
