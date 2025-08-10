0f279bae0733ef13ea21db95d4cf5390bf3475a0
Fantasy <1362724990@qq.com>
2025-08-10 19:09:25 +0800

    feat: MCP 协议 1. 恋爱大师应用整合高德地图 MCP 服务，能够推荐约会地点 2. 开发图片搜索 MCP Server

---
+++ rabbit-image-search-mcp-server/src/test/java/com/fantasy/rabbitimagesearchmcpserver/RabbitImageSearchMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fantasy.rabbitimagesearchmcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitImageSearchMcpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
